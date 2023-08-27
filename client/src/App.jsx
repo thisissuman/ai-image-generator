df08ff3be95f413b5273047073c4493e74d90a62
thisissuman <43521912+thisissuman@users.noreply.github.com>
2023-08-27 22:01:18 +0530

    4th

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import { Home, CreatePost } from "./pages";
 const App = () => {
   return (
     <BrowserRouter>
-      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#d2ddf1]">
+      <header className="w-full flex justify-between items-center bg-[#34D399] sm:px-8 px-4 py-4 border-b border-b-[#d2ddf1]">
         <Link to="/">
           <img
             src={logo_1}
@@ -15,12 +15,12 @@ const App = () => {
         </Link>
         <Link
           to="/create-post"
-          className="font inter font-medium bg-[#d946ef] text-white px-4 py-2 rounded-md"
+          className="font inter font-medium bg-[#F0ABFC] text-white px-4 py-2 rounded-md"
         >
           Created
         </Link>
       </header>
-      <main className="sm:p-8 px-4 py-8 w-full bg-[#d946ef] min-h-[calc(100vh-73px)]">
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#F0ABFC] min-h-[calc(100vh-73px)]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-post" element={<CreatePost />} />
